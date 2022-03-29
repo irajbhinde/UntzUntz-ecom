4725f79ace5e14dea234bd030c3002a0a0b363eb
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-03-29 23:59:34 +0530

    refactor: using prettier

--- src/components/HomePage/HomePageCard/HomePageCard.jsx
+++ src/components/HomePage/HomePageCard/HomePageCard.jsx
@@ -1,73 +1,19 @@
 import { Link } from "react-router-dom";
 import { categories } from "../../../backend/db/categories";
 
-export default function HomePageCard(){
-    return(
-        categories.map(({categoryName, image}) => (
-            <div class="home-cards">
-                   <Link to="/productlisting">
-                        <div class="cards textovermedia">
-                            <div class="img_text">
-                                <div class="cards-image">
-                                    <img src={image} />
-                                </div>
-                                <div class="cards-title onimage">
-                                    <h4 class="strip">{categoryName}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
-                    {/* <Link to="/productlisting">
-                        <div class="cards textovermedia">
-                            <div class="img_text">
-                                <div class="cards-image">
-                                    <img src={image} />
-                                </div>
-                                <div class="cards-title onimage">
-                                    <h4 class="strip">{categoryName}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link to="/productlisting">
-                        <div class="cards textovermedia">
-                            <div class="img_text">
-                                <div class="cards-image">
-                                    <img src={image} />
-                                </div>
-                                <div class="cards-title onimage">
-                                    <h4 class="strip">{categoryName}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link to="/productlisting">
-                        <div class="cards textovermedia">
-                            <div class="img_text">
-                                <div class="cards-image">
-                                    <img src={image} />
-                                </div>
-                                <div class="cards-title onimage">
-                                    <h4 class="strip">{categoryName}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link to="/productlisting">
-                        <div class="cards textovermedia">
-                            <div class="img_text">
-                                <div class="cards-image">
-                                    <img src={image} />
-                                </div>
-                                <div class="cards-title onimage">
-                                    <h4 class="strip">{categoryName}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </Link> */}
-                </div>
-                ))
-                
-    )
-    
+export default function HomePageCard() {
+  return categories.map(({ categoryName, image }) => (
+    <Link to="/productlisting">
+      <div className="cards textovermedia">
+        <div className="img_text">
+          <div className="cards-image">
+            <img src={image} />
+          </div>
+          <div className="cards-title onimage">
+            <h4 className="strip">{categoryName}</h4>
+          </div>
+        </div>
+      </div>
+    </Link>
+  ));
 }
--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import landing_image_mellowhead from "../../assets/landing_page_images/landing_page_mellowhead.svg"
-import "./landingPage.css"
-
+import landing_image_mellowhead from "../../assets/landing_page_images/landing_page_mellowhead.svg";
+import "./landingPage.css";
 import Nav from "../../components/Nav/Nav";
 
-
-export default function LandingPage (){
-    return (
-        <>
-        <Nav />
-        <div className="landing-content">
-                <div className="landing-text">
-                    <p>Your Neighbors Will Listen To Good Music Whether They Like It Or Not</p>
-                    <Link to="/signup"><button className="btn-getstarted">Get Started</button></Link>
-                </div>
-                <div className="landing-image">
-                    <img src={landing_image_mellowhead} alt="Error loading" />
-                </div>
-
-            </div></>
-    )
+export default function LandingPage() {
+  return (
+    <>
+      <Nav />
+      <div className="landing-content">
+        <div className="landing-text">
+          <p>
+            Your Neighbors Will Listen To Good Music Whether They Like It Or Not
+          </p>
+          <Link to="/signup">
+            <button className="btn-getstarted">Get Started</button>
+          </Link>
+        </div>
+        <div className="landing-image">
+          <img src={landing_image_mellowhead} alt="Error loading" />
+        </div>
+      </div>
+    </>
+  );
 }
