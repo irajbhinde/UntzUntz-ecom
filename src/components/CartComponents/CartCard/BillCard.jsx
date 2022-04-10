bb22cb6e29736410fef8fccdf42624f601732c65
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-04-10 16:48:06 +0530

    feat: Cart Total Price

--- src/components/CartComponents/CartCard/BillCard.jsx
+++ src/components/CartComponents/CartCard/BillCard.jsx
@@ -1,11 +1,19 @@
+import { useCart } from "../../context/cart-context"
 
-export default function BillCard () {
+export default function BillCard ({product}) {
+    const {cartState, cartDispatch} = useCart();
+    const {cartProducts} = cartState;
+    console.log("FOR PRICE", cartProducts);
+    const cartTotal = cartProducts.reduce(
+        (prev,curr) => prev + curr.price*curr.quantity,0   
+    )
+    
     return (
         <div className="bill-card flex_c">
-                <h3>PRICE DETAILS (2 Items)</h3>
+                <h3>PRICE DETAILS ({cartProducts.length} Items)</h3>
                 <div className="bill-container flex_r">
                     <p>Total MRP</p>
-                    <p>₹6000</p>
+                    <p>₹{cartTotal}</p>
                 </div>
                 <div className="bill-container flex_r">
                     <p>Discount on MRP</p>
@@ -17,11 +25,11 @@ export default function BillCard () {
                 </div>
                 <div className="bill-container border-bottom flex_r">
                     <p>Convenience Fee</p>
-                    <p>₹30</p>
+                    <p>₹0</p>
                 </div>
                 <div className="bill-container total-amount flex_r">
                     <p>Total Amount</p>
-                    <p>₹6030</p>
+                    <p>₹{cartTotal}</p>
                 </div>
                 <a href="/Pages/Checkout/checkout.html"
                 ><button className="btn checkout primary-btn">Checkout</button></a>
