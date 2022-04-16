8e41854f4736f5f58b6104be6d600b9f6030de9d
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-04-17 02:14:33 +0530

    feat : 404 page & empty cart / wishlist images

--- src/pages/wishlist/WishListPage.jsx
+++ src/pages/wishlist/WishListPage.jsx
@@ -1,18 +1,29 @@
 import { CartNav } from "../../components";
 import { useCart } from "../../components/context/cart-context";
 import WishListCard from "../../components/WishListComponents/WishListCard/WishListCard";
+import { empty_wishlist_image } from "../../assets";
 
 export default function WishListPage() {
-    const {cartState, cartDispatch} = useCart();
-    const {wishlistProducts} = cartState;
-    return (
-        <><CartNav /><div class="doc-wishlist flex_c">
-            <h1>My Wishlist</h1>
-            <div class="wishlist-cards flex_r">
-                {wishlistProducts.map((product) => {
-                    return <WishListCard product={product} key={product._id} />;
-                })}
-            </div>
-        </div></>
-    )
+  const { cartState, cartDispatch } = useCart();
+  const { wishlistProducts } = cartState;
+  return (
+    <>
+      <CartNav />
+      <div class="doc-wishlist flex_c">
+        <h1>My Wishlist</h1>
+        {wishlistProducts.length === 0 ? (
+          <div className="empty_wishlist_img">
+            <img src={empty_wishlist_image} alt="error" />
+            <h2 className="wishlist-msg">Your wishlist is empty</h2>
+          </div>
+        ) : (
+          <div class="wishlist-cards flex_r">
+            {wishlistProducts.map((product) => {
+              return <WishListCard product={product} key={product._id} />;
+            })}
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
